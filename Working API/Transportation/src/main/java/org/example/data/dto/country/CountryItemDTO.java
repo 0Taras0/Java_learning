cfa6b8e8dc22f127dfdf93716fbc006464967ee5
0Taras0 <rringoo043@gmail.com>
2025-11-18 20:58:25 +0200

    Begin Java API

---
+++ Working API/Transportation/src/main/java/org/example/data/dto/country/CountryItemDTO.java
@@ -0,0 +1,12 @@
+package org.example.data.dto.country;
+
+import lombok.Data;
+
+@Data
+public class CountryItemDTO {
+    private Long id;
+    private String name;
+    private String code;
+    private String slug;
+    private String dateCreated;
+}
---
+++ Working API/react-tran/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MainHeader from "./layouts/Main/MainHeader.tsx";
+
+function App() {
+
+  return (
+    <>
+        <MainHeader/>
+        <h1>Hello React</h1>
+    </>
+  )
+}
+
+export default App
